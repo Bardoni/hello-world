c969f66b07a86cbb271101e0ee693bc9e516504e
Bardoni <91908275+Bardoni@users.noreply.github.com>
2021-10-05 11:08:30 -0300

    Add files via upload

--- PruebasP1PAPB2/tema1/tema1/Biblioteca.java
+++ PruebasP1PAPB2/tema1/tema1/Biblioteca.java
@@ -1,11 +1,36 @@
 package tema1;
 
 public class Biblioteca {
-
+	
+	Libro libros [] = new Libro[1000];
+	prestamos prestamosHistoricos[] = new prestamos[5000];
+	
 	public static void main(String[] args) {
 		
-		prestamos prestamosHistoricos [] =new prestamos [];
+	}
+	
+	public boolean prestar(Libro libro, estudiante estudiante) {
+		if (estudiante.tomarPrestamo() && libro.isDisponible()) {
+			libro.prestar();
+			
+			for (int i = 0; i < prestamosHistoricos.length; i++) {
+				if (prestamosHistoricos[i]==null) {
+					prestamos prestamo = new prestamos(estudiante.getDni(), libro.getCodigo());
+					prestamosHistoricos[i]=prestamo;
+				break;
+				}
+				
+			}
+			
+			return true;
+		}else return false;
 		
 	}
-
+	
+	public boolean imprimir(Libro libro) {
+		if (!libro.isFotocopiable().startsWith("no")) {
+			return true;
+		}else return false;
+	}
+	
 }
--- PruebasP1PAPB2/tema1/tema1/prestamos.java
+++ PruebasP1PAPB2/tema1/tema1/prestamos.java
@@ -1,8 +1,22 @@
 package tema1;
 
 public class prestamos {
-
+private int codigo;
+private int contador=0;
 	int dniEstudiante;
 	int codLibro;
-	
+	public int getDniEstudiante() {
+		return dniEstudiante;
+	}
+	public int getCodLibro() {
+		return codLibro;
+	}
+	public prestamos(int dniEstudiante, int codLibro) {
+		this.codigo = ++ contador;
+		this.dniEstudiante = dniEstudiante;
+		this.codLibro = codLibro;
+	}
+	public int getCodigo() {
+		return codigo;
+	}
 }
